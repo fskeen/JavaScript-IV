e7d2a8856369fbc9e6fa3c6d4844c6e9811f9e21
Faye Skeen <fskeen@gmail.com>
2019-06-13 11:39:25 -0700

    Added 3 arrays of randomly generated students, PMs, and instructors.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,98 @@
 // CODE here for your Lambda Classes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const instructors = [{
+    "id": 1,
+    "first_name": "Rakel",
+    "last_name": "Millwall",
+    "location": "Danzi",
+    "age": 78,
+    "favLanguage": "CSS",
+    "specialty": "quam sollicitudin vitae",
+    "catchphrase": "Aenean auctor gravida sem."
+  }, {
+    "id": 2,
+    "first_name": "Chadwick",
+    "last_name": "Limer",
+    "location": "Safonovo",
+    "age": 60,
+    "favLanguage": "Java",
+    "specialty": "at",
+    "catchphrase": "Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante."
+  }, {
+    "id": 3,
+    "first_name": "Abbe",
+    "last_name": "Muldoon",
+    "location": "San Agustin",
+    "age": 26,
+    "favLanguage": "C#",
+    "specialty": "in faucibus orci",
+    "catchphrase": "Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus."
+  }]
+
+  let students = [{
+    "id": 1,
+    "first_name": "Garold",
+    "last_name": "Korf",
+    "location": "Bailadores",
+    "age": 84,
+    "previousBackground": "Assistant Media Planner",
+    "className": "ART 216",
+    "favSubjects": "['Html', 'CSS', 'JavaScript']"
+  }, {
+    "id": 2,
+    "first_name": "June",
+    "last_name": "Vampouille",
+    "location": "Luleå",
+    "age": 67,
+    "previousBackground": "Marketing Manager",
+    "className": "HIST 324",
+    "favSubjects": "['Labs', 'FlexBox', 'JavaScript']"
+  }, {
+    "id": 3,
+    "first_name": "Rosalia",
+    "last_name": "Woollett",
+    "location": "Nangewer",
+    "age": 58,
+    "previousBackground": "Occupational Therapist",
+    "className": "CS 103",
+    "favSubjects": "['Node', 'FlexBox', 'React']"
+  }]
+
+  const pms = [{
+    "id": 1,
+    "first_name": "Brooke",
+    "last_name": "Kochl",
+    "location": "Älmhult",
+    "age": 20,
+    "gradClassName": "WEB 18",
+    "favInstructor": "Annabelle"
+  }, {
+    "id": 2,
+    "first_name": "Kris",
+    "last_name": "Riddoch",
+    "location": "Kislyakovskaya",
+    "age": 41,
+    "gradClassName": "WEB 6",
+    "favInstructor": "Alix"
+  }, {
+    "id": 3,
+    "first_name": "Gun",
+    "last_name": "Gull",
+    "location": "Bogovarovo",
+    "age": 87,
+    "gradClassName": "WEB 4",
+    "favInstructor": "Keelby"
+  }]
