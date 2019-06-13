05c1766fc4e36a0653e5e6d236b8f6bc96eb0a0d
Faye Skeen <fskeen@gmail.com>
2019-06-13 11:56:54 -0700

    Added the class contructors for instructors, students, and PMs.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,9 +1,71 @@
 // CODE here for your Lambda Classes
 
+class Person {
+    constructor(personStats) {
+        this.name = `${personStats.first_name} ${personStats.lastname}`;
+        this.age = personStats.age;
+        this.location = personStats.location;
+    }
+
+    speak() {
+        console.log(`Hello, my name is ${this.name}. I am from ${this.location}.`)
+    }
+}
 
 
+class Instructor extends Person {
+    constructor(instructorStats) {
+        super(instructorStats);
+        this.specialty = instructorStats.specialty;
+        this.favLanguage = instructorStats.favLanguage;
+        this.catchphrase = instructorStats.catchphrase;
+    }
 
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}!`);
+    }
 
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score in ${subject}!`);
+    }
+};
+
+class Student extends Person {
+    constructor(studentStats) {
+        super(studentStats);
+        this.previousBackground = studentStats.previousBackground;
+        this.className = studentStats.className;
+        this.favSubjects = studentStats.favSubjects;
+    }
+
+    listsSubjects(subjects) {
+        console.log(`${this.name}'s favorite subjects are ${subjects[0]}, ${subjects[1]}, and ${subjects[2]}.`)
+    }
+
+    prAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}.`);
+    }
+
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun the Sprint Challenge on ${subject}.`);
+    }
+}
+
+class ProjectManager extends Person {
+    constructor(pmStats) {
+        super(pmStats);
+        this.gradClassName = pmStats.gradClassName;
+        this.favInstructor = pmStats.favInstructor;
+    }
+
+    standUp(channel) {
+        console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+    }
+
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}.`);
+    }
+}
 
 
 
